441b1ba389e3619525033d8d8807e4e8e3468bca
Verona <vdanessi@gmail.com>
2020-02-06 12:32:54 -0300

    primer trabajo javascript

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var h2=document.getElementById('form-signin-heading');
+h2.innerHTML="Por favor inicia sesión";
+
+var inputmail=document.getElementById('inputEmail');
+inputmail.placeholder="Correo Electrónico";
+
+var inputpassword=document.getElementById('inputPassword');
+inputpassword.placeholder="Contraseña";
+
+var recuerdo=document.getElementsByTagName("span")[0];
+recuerdo.innerHTML="Recordar datos";
+
+var button=document.getElementsByTagName("button")[0];
+button.innerHTML="Iniciar Sesión";
+
+
